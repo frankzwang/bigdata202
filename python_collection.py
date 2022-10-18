5a1c5120d734facacd2329602e786a5816f0a64d
Frank Wang <frankwang711@gmail.com>
2022-10-17 22:31:02 -0700

    Update python_collection.py
    add rank for players

--- python_collection.py
+++ python_collection.py
@@ -1,3 +1,5 @@
+from collections import Counter
+from collections import OrderedDict
 #group by value for dict pairs######################################
 def group_by_sources(files):
 
@@ -19,3 +21,26 @@ if __name__ == "__main__":
         'visa.json': 'dow30'
     }   
     print(group_by_sources(files))
+
+    #Class (advanced): rank scores with multi factors for sports or other events          ##########################################################<
+class PlayerRank:
+    def __init__(self, players):
+        self.standings = OrderedDict([(player, Counter()) for player in players])
+
+    def record_result(self, player, score):
+        self.standings[player]['games_played'] += 1
+        self.standings[player]['score'] += score
+
+    def player_rank(self, rank):
+        ranks = [(-counter['score'], counter['games_played'], i, name)
+                 for i, (name, counter) in enumerate(self.standings.items())]
+
+        return sorted(ranks)[rank-1][3]
+
+
+table = PlayerRank(['William', 'Henry', 'Luke'])
+table.record_result('William', 2)
+table.record_result('William', 3)
+table.record_result('Henry', 5)
+table.record_result('Luke', 5)
+print(table.player_rank(1))
