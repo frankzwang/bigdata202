81b9a433fb6975852d9aeeee975fbd5bcd41cd63
Frank Wang <frankwang711@gmail.com>
2022-10-31 23:26:46 -0700

    Update sql_scripts.sql
    add avg, min, max SQL scripts

--- sql_scripts.sql
+++ sql_scripts.sql
@@ -13,3 +13,13 @@ SELECT * from tblGrade
 SELECT studentID, AVG(score) AS AavgScorePerStudent
 FROM tblGrade
 GROUP BY studentID
+
+--Extract (full row with gradeID) the lowest grade ever received from a student in any given course
+SELECT * FROM tblGrade
+WHERE score = (SELECT MIN(score) FROM tblGrade)
+
+--Get the maximum grade per course (full row with gradeID).
+SELECT * FROM tblGrade
+SELECT courseID, MAX(score) AS maxScorePerCourse 
+FROM tblGrade
+GROUP BY courseID
