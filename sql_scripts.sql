328d782abc969136f48b5cc27366b04dbd5e93f3
Frank Wang <frankwang711@gmail.com>
2023-06-25 23:03:53 -0700

    find duplicate user name
    Find all duplicate user name  in table using window function

--- sql_scripts.sql
+++ sql_scripts.sql
@@ -73,6 +73,7 @@ WHERE activityAt >= DateAdd(Day,- 30, GetDate())
 GROUP BY [studentID]
 HAVING (COUNT(activityAt)) < 5
 
+--------------------------------------------------------------------------------------------------------------------------------------------------
 --RECURSIVE CTE case 1: print out Factorial (MySQL dialect) 
 WITH RECURSIVE cteFact as 
 (
@@ -85,6 +86,7 @@ WITH RECURSIVE cteFact as
  SELECT n, fact					--result
  FROM cteFact
 
+---------------------------------------------------------------------------------------------------------------------------------------------------
 --RECURSIVE CTE case 2: Corporate Hierarchical 
 --print out employee ID and their manager ID (SQL Server dialect) 
 WITH cteEmp AS
@@ -100,3 +102,14 @@ WITH cteEmp AS
 SELECT *
 FROM cteEmp
 ORDER BY emp_id, manager;
+
+---------------------------------------------------------------------------------------------------------------------------------------------------
+-- Problem: Find all duplicate user name  in table
+-- Solution: Using window function
+select user_id, user_name, email
+from (
+select *,
+row_number() over (partition by user_name order by user_id) as rn
+from users u
+order by user_id) x
+where x.rn <> 1;
